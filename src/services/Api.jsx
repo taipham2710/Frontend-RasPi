f6a82d8d0ba4daab9295c1102a5d0d11c5811d70
taipham2710 <taipham.dev@gmail.com>
2025-07-06 15:53:22 +0700

    feat(api): add getLatestLogByType and triggerUpdateDevice functions

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -10,3 +10,6 @@ export const getLogsByDevice = (deviceId) => axios.get(`${API_URL}/api/device/${
 export const deleteDevice = (id) => axios.delete(`${API_URL}/api/device/${id}`);
 export const updateDevice = (id, data) => axios.put(`${API_URL}/api/device/${id}`, data);
 export const deleteLog = (id) => axios.delete(`${API_URL}/api/log/${id}`);
+
+export const getLatestLogByType = (deviceId, type) => axios.get(`${API_URL}/api/device/${deviceId}/latest-log?type=${type}`);
+export const triggerUpdateDevice = (deviceId) => axios.post(`${API_URL}/api/device/${deviceId}/update`);
